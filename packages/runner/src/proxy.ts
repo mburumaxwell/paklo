7c4a3983b2dee16918f345c0a62142a3eb0a511e
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2026-02-11 15:02:11 +0000

    Update proxy command to use `dependabot-proxy` (#2522)
    This was forgotten in https://github.com/mburumaxwell/paklo/commit/e7f4e1b1d70e17088441ed9c9f3689baed05ecb9

--- packages/runner/src/proxy.ts
+++ packages/runner/src/proxy.ts
@@ -212,7 +212,7 @@ export class ProxyBuilder {
         `ACTIONS_ID_TOKEN_REQUEST_TOKEN=${process.env.ACTIONS_ID_TOKEN_REQUEST_TOKEN || ''}`,
         `ACTIONS_ID_TOKEN_REQUEST_URL=${process.env.ACTIONS_ID_TOKEN_REQUEST_URL || ''}`,
       ],
-      Entrypoint: ['sh', '-c', '/usr/sbin/update-ca-certificates && /update-job-proxy'],
+      Entrypoint: ['sh', '-c', '/usr/sbin/update-ca-certificates && /dependabot-proxy'],
 
       HostConfig: {
         NetworkMode: internalNetworkName,
