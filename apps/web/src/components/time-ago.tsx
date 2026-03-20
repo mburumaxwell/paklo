a36931d5485013ce60b647bff7b4390fc557a87b
Maxwell Weru <mburumaxwell@gmail.com>
2026-03-20 14:44:37 +0000

    Replace react-timeago with own logic and date-fns and a tooltip

--- apps/web/src/components/time-ago.tsx
+++ apps/web/src/components/time-ago.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useCallback, useMemo } from 'react';
-import { default as BaseTimeAgo, type Formatter, type Suffix, type Unit } from 'react-timeago';
-import { makeIntlFormatter } from 'react-timeago/defaultFormatter';
+import { intlFormatDistance } from 'date-fns';
+import * as React from 'react';
 
-export type TimeAgoProps = {
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+
+export interface TimeAgoProps extends Pick<React.ComponentPropsWithoutRef<'span'>, 'className'> {
   /**
    * The Date to display.
    * An actual Date object or something that can be fed to new Date
@@ -23,45 +24,99 @@ export type TimeAgoProps = {
    * @default 7 days.
    */
   cutoff?: number;
-};
 
-export function TimeAgo({ value, locale, cutoff = 7 * 24 * 60 * 60 * 1000, ...props }: TimeAgoProps) {
+  /**
+   * Whether to show the tooltip with exact date and time on hover.
+   * @default true
+   */
+  showTooltip?: boolean;
+}
+
+export function TimeAgo({
+  value,
+  locale,
+  cutoff = 7 * 24 * 60 * 60 * 1000,
+  showTooltip = true,
+  className,
+  ...props
+}: TimeAgoProps) {
   const date = new Date(value);
+  const ms = date.getTime();
+  const isValidDate = Number.isFinite(ms);
 
-  const intlFormatter = useMemo(
-    () =>
-      makeIntlFormatter({
-        locale, // string
-        localeMatcher: 'best fit', // 'lookup' | 'best fit',
-        numberingSystem: 'latn', // Intl$NumberingSystem such as 'arab', 'deva', 'hebr' etc.
-        style: 'long', // 'long' | 'short' | 'narrow',
-        numeric: 'auto', //  'always' | 'auto', Using 'auto` will convert "1 day ago" to "yesterday" etc.
-      }),
-    [locale],
-  );
+  const [now, setNow] = React.useState(() => Date.now());
 
-  const absFormatter = useMemo(
+  const absoluteFormatter = React.useMemo(
     () => new Intl.DateTimeFormat(locale, { year: 'numeric', month: 'short', day: '2-digit' }),
     [locale],
   );
 
-  const formatter = useCallback(
-    (
-      value: number,
-      unit: Unit,
-      suffix: Suffix,
-      epochMilliseconds: number,
-      nextFormatter: Formatter,
-      now: () => number,
-    ) => {
-      const age = now() - date.getTime();
-      if (age >= cutoff) return absFormatter.format(date);
-
-      // delegate to Intl relative formatter
-      return intlFormatter(value, unit, suffix, epochMilliseconds, nextFormatter, now);
-    },
-    [absFormatter, intlFormatter, cutoff, date],
+  const [localDateTimeFormatter, utcDateTimeFormatter] = React.useMemo(
+    () => [
+      new Intl.DateTimeFormat(locale, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short',
+      }),
+      new Intl.DateTimeFormat(locale, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZone: 'UTC',
+        timeZoneName: 'short',
+      }),
+    ],
+    [locale],
   );
 
-  return <BaseTimeAgo formatter={formatter} date={date} {...props} />;
+  const age = isValidDate ? Math.abs(now - ms) : 0;
+  const refreshInterval = age < 60_000 ? 1_000 : age < 3_600_000 ? 60_000 : age < 86_400_000 ? 3_600_000 : 86_400_000;
+
+  React.useEffect(() => {
+    if (!isValidDate) return;
+    const intervalId = window.setInterval(() => setNow(Date.now()), refreshInterval);
+    return () => window.clearInterval(intervalId);
+  }, [isValidDate, refreshInterval]);
+
+  if (!isValidDate) return null;
+
+  const text =
+    age >= cutoff
+      ? absoluteFormatter.format(date)
+      : intlFormatDistance(date, now, { locale, numeric: 'auto', style: 'long' });
+  const localDateTime = localDateTimeFormatter.format(date);
+  const utcDateTime = utcDateTimeFormatter.format(date);
+
+  function TimeComponent() {
+    return (
+      <time className={className} dateTime={date.toISOString()} suppressHydrationWarning {...props}>
+        {text}
+      </time>
+    );
+  }
+
+  if (!showTooltip) return <TimeComponent />;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <TimeComponent />
+      </TooltipTrigger>
+      <TooltipContent className='max-w-none py-2'>
+        <div className='grid min-w-64 grid-cols-[auto_1fr] gap-x-3 gap-y-1'>
+          <div className='rounded bg-background/10 px-1.5 py-0.5 font-medium tracking-wide uppercase'>Local</div>
+          <div>{localDateTime}</div>
+          <div className='rounded bg-background/10 px-1.5 py-0.5 font-medium tracking-wide uppercase'>UTC</div>
+          <div>{utcDateTime}</div>
+        </div>
+      </TooltipContent>
+    </Tooltip>
+  );
 }
